03fe15f8ce8364118d0c8698c93e891e404cfd82
wobloj <wiktorobloj1@gmail.com>
2024-02-03 13:52:33 +0100

    full

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ let favouriteLocationTemp;
 const favouriteTable = document.getElementById("favourite-table")
 const favTd = document.getElementById("fav")
 
+const infoBox = document.getElementById('info-box')
+
+let isAnimationEnd = true;
+
 async function loadWeatherData(location){
     const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${location}&lang=pl`;
     const options = {
@@ -30,11 +34,11 @@ async function loadWeatherData(location){
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        console.log(result);
         setWeatherData(location, result.current.temp_c, result.current.feelslike_c, result.current.cloud, result.current.wind_kph, result.current.wind_dir, result.current.humidity, result.current.condition.text, result.current.condition.icon);
         favouriteLocationTemp = location;
         document.title = location;
         favicon.href = result.current.condition.icon;
+        favouriteButton.style.display = "block"
     } catch (error) {
         currTemp.innerHTML="Nie znaleziono lokalizacji"
         currLocation.innerHTML = '';
@@ -60,50 +64,94 @@ function setWeatherData(location, temp_c, feelslike_c, cloud, wind, wind_dir, hu
     searchingQuery.value = ''
 }
 
-function existInFavourite(storage, location){
-    if(location != ''){
-        for (let i = 0; i < storage.length; i++) {
-            if(localStorage.getItem(i) == location){
-                return true
+function existInFavourite(storage, location) {
+    if (location !== undefined && location !== '') {
+        for (let key in storage) {
+            if (storage.hasOwnProperty(key) && localStorage.getItem(key) === location) {
+                return true;
             }
+            displayInfo("exist", location)
         }
+        
+        return false;
     }
-    return 
+    return false;
 }
 
 function addToFavourite(location){
-    if(!existInFavourite(localStorage, location)){
-        localStorage.setItem(localStorage.length, location);
-        
+    if(existInFavourite(localStorage, location) === false){
+        let id = uuidv4();
+        displayInfo("add", location)            
+        localStorage.setItem(id, location);
+        loadTable()
     }
 }
 
 function deleteFromFavourite(value){
-    console.log(value);
+    displayInfo("delete", localStorage.getItem(value))
     localStorage.removeItem(value)
-    location.reload(true)
+    loadTable()
 }
 
 function loadTable(){
+    favouriteTable.innerHTML = ''
+
     Object.keys(localStorage).forEach(key =>{
         favouriteTable.innerHTML += 
         `
         <tr>
-            <td id="delete" onclick="deleteFromFavourite(${key})"><i class="fa-regular fa-trash-can"></i></td>
+            <td id="delete" onclick="deleteFromFavourite('${key}')"><i class="fa-regular fa-trash-can"></i></td>
             <td id="fav" class="select-fav" onclick="loadWeatherData('${localStorage.getItem(key)}')">${localStorage.getItem(key)}</td>
         </tr>
         `
     })
 }
 
-function addToTable(id, location){
-    favouriteTable.innerHTML += 
-    `
-    <tr>
-        <td id="delete" onclick="deleteFromFavourite(${id})"><i class="fa-regular fa-trash-can"></i></td>
-        <td id="fav" class="select-fav">${location}</td>
-    </tr>
-    `
+function animationPopOut(){
+    if(isAnimationEnd){
+        isAnimationEnd = false
+        infoBox.classList.add("anim-in");
+        setTimeout(() => {
+            infoBox.classList.remove("anim-in");
+            infoBox.classList.add("anim-out");
+        }, 3000);
+        infoBox.classList.remove("anim-out");
+    }else{
+        return;
+    }
+    
+    infoBox.addEventListener("animationend" , () =>{
+        isAnimationEnd = true;
+    });
+}
+
+function displayInfo(type, location){
+    switch (type) {
+        case "delete":
+            infoBox.innerHTML = `<p>Lokacja <span style="color: rgb(255, 106, 106);">${location}</span>, została usunięta z ulubionych</p>`
+            animationPopOut()
+            break;
+
+        case "add":
+            infoBox.innerHTML = `<p>Lokacja <span style="color: rgb(145, 252, 172);">${location}</span>, została dodana do ulubionych</p>`
+            animationPopOut()
+            break;
+
+        case "exist":
+            infoBox.innerHTML = `<p>Lokacja <span style="color: rgb(252, 241, 145);">${location}</span>, już jest dodana w twoich ulubionych</p>`
+            animationPopOut()
+            break;
+        
+        default:
+            
+            break;
+    }
+}
+
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
 }
 
 favouriteButton.addEventListener("click", ()=>{
